8b45e3025727b0f3b25bd6ec4e55d103c11fd2d5
bkraffa <brunocaraffa@gmail.com>
2023-07-23 01:16:44 +0000

    working on the model monitoring

--- orchestrate.py
+++ orchestrate.py
@@ -19,9 +19,11 @@ def read_data(path):
 @task
 def pre_processing(df):
     df.dropna(inplace=True)
+    df.started_at = df.started_at.astype('datetime64[ns]')
+    df.ended_at = df.ended_at.astype('datetime64[ns]')
     df['start_end_id'] = df['start_station_id'] + '-' +df['end_station_id']
     df = df[(df.duration >= 1) & (df.duration <= 60)]
-    columns = ['ride_id','started_at','ended_at','start_station_name','end_station_name','start_station_id','end_station_id',
+    columns = ['start_station_name','end_station_name','start_station_id','end_station_id',
                'start_lat','start_lng','end_lat','end_lng']
     df.drop(columns=columns, inplace=True)
     df.reset_index(drop=True, inplace=True)
@@ -31,16 +33,14 @@ def pre_processing(df):
 def training_preparation(df):
     categorical = ['rideable_type','member_casual','start_end_id']
 
-    X = df.drop(columns=['duration'])
-    y = df.duration
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=42)
+    train_data = df.loc[df['started_at'] < '2023-03-01 00:00:00' ]
+    test_data = df.loc[df['started_at'] > '2023-03-01 00:00:00' ]
 
     dv = DictVectorizer()
 
-    train_dicts = X_train[categorical].to_dict(orient='records')
+    train_dicts = train_data[categorical].to_dict(orient='records')
     X_train = dv.fit_transform(train_dicts)
-    test_dicts = X_test[categorical ].to_dict(orient='records')
+    test_dicts = test_data[categorical].to_dict(orient='records')
     X_test = dv.transform(test_dicts)
 
     pathlib.Path('models').mkdir(exist_ok=True)
@@ -48,7 +48,7 @@ def training_preparation(df):
             pickle.dump(dv,f_out)
     mlflow.log_artifact('models/preprocessor.b',artifact_path="dv_preprocessor")
 
-    return X_train, X_test, y_train, y_test  
+    return X_train, train_data, X_test, test_data  
 
 @task()
 def train(X_train,y_train):
@@ -67,18 +67,24 @@ def calculate_mse(model,X_test,y_test):
 def main_flow():
     warnings.filterwarnings("ignore")    
 
-    TRACKING_SERVER_HOST = "ec2-18-217-76-61.us-east-2.compute.amazonaws.com" #change if ec2 instance is reinitiated
+    TRACKING_SERVER_HOST = "ec2-18-188-25-201.us-east-2.compute.amazonaws.com" #change if ec2 instance is reinitiated
     mlflow.set_tracking_uri(f"http://{TRACKING_SERVER_HOST}:5000")
     mlflow.set_experiment("chicago-bike-share")
 
     with mlflow.start_run():
         df = read_data('s3://chicago-bike-trips/dataset.parquet')
         df = pre_processing(df)
-        X_train, X_test, y_train, y_test  = training_preparation(df)
-        model = train(X_train,y_train)
+        X_train, train_data, X_test, test_data  = training_preparation(df)
+
+        train_data.to_parquet('model_monitoring/data/reference.parquet')
+        test_data.to_parquet('model_monitoring/data/current.parquet')
+
+        model = train(X_train,train_data['duration'])
         mlflow.sklearn.log_model(model, artifact_path='model')
-        rmse = calculate_mse(model,X_test,y_test)
-        mlflow.log_metric("rmse", rmse)
+        train_rmse = calculate_mse(model,X_train,train_data['duration'])
+        mlflow.log_metric("train_rmse", train_rmse)        
+        test_rmse = calculate_mse(model,X_test,test_data['duration'])
+        mlflow.log_metric("test_rmse", test_rmse)
     mlflow.end_run()
 
 if __name__ == "__main__":
